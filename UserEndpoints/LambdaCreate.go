83b592d76f9ce374a4831989ccca1d5dfbfe3ffc
Kaeden Le <kaedenle@gmail.com>
2023-08-25 23:53:08 -0400

    Added beta endpoint files for register and login

---
+++ UserEndpoints/LambdaCreate.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"log"
+	"os"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
+	"github.com/aws/aws-sdk-go/aws"
+)
+
+type User struct {
+	UserID   string   `json:"UserID"`
+	Email    string   `json:"email"`
+	Password string   `json:"password"`
+	Schedule []string `json:"schedule"`
+	Username string   `json:"username"`
+}
+
+type UserSearch struct {
+	UserID   string `json:"UserID"`
+	UseUser  bool   `json:"useUser"`
+	Username string `json:"username"`
+}
+
+var table string
+var client *dynamodb.Client
+
+func init() {
+	table = os.Getenv("TABLE_NAME")
+
+	if table == "" {
+		log.Fatal("missing environment variable TABLE_NAME")
+	}
+	cfg, _ := config.LoadDefaultConfig(context.Background())
+	client = dynamodb.NewFromConfig(cfg)
+}
+
+func main() {
+	lambda.Start(handler)
+}
+
+/*func handler(ctx context.Context, event events.APIGatewayV2HTTPRequest) (events.APIGatewayV2HTTPResponse, error) {
+
+	payload := event.Body
+	log.Println("payloads", payload)
+
+	return events.APIGatewayV2HTTPResponse{StatusCode: http.StatusOK}, nil
+}*/
+
+func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	search := UserSearch{}
+	log.Println(request.Body)
+
+	if request.Body == "" {
+		return events.APIGatewayProxyResponse{Body: request.Body}, errors.New("no body found")
+	}
+
+	err := json.Unmarshal([]byte(request.Body), &search)
+
+	if err != nil {
+		return events.APIGatewayProxyResponse{}, err
+	}
+
+	log.Println("payloads: ", search)
+	item := make(map[string]types.AttributeValue)
+
+	item["UserID"] = &types.AttributeValueMemberS{Value: search.UserID}
+
+	TheInput, err := client.GetItem(context.Background(), &dynamodb.GetItemInput{
+		TableName: aws.String(table),
+		Key:       item,
+	})
+
+	if err != nil {
+		return events.APIGatewayProxyResponse{}, err
+	}
+
+	newUser := User{}
+
+	attributevalue.UnmarshalMap(TheInput.Item, &newUser)
+
+	js, err := json.Marshal(newUser)
+
+	if err != nil {
+		return events.APIGatewayProxyResponse{}, err
+	}
+
+	log.Println(newUser)
+	response := events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       string(js),
+	}
+
+	return response, nil
+}
---
+++ UserEndpoints/LambdaVerify.go
@@ -0,0 +1,115 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
+	"github.com/aws/aws-sdk-go/aws"
+)
+
+type User struct {
+	UserID   string   `json:"UserID"`
+	Email    string   `json:"email"`
+	Password string   `json:"password,omitempty"`
+	Schedule []string `json:"schedule"`
+	Username string   `json:"username"`
+	Name     string   `json:"name"`
+}
+
+var table string
+var client *dynamodb.Client
+
+func init() {
+	table = os.Getenv("TABLE_NAME")
+
+	if table == "" {
+		log.Fatal("missing environment variable TABLE_NAME")
+	}
+	cfg, _ := config.LoadDefaultConfig(context.Background())
+	client = dynamodb.NewFromConfig(cfg)
+}
+
+func main() {
+	lambda.Start(handler)
+}
+
+func unpackRequest(body string) User {
+	if body == "" {
+		return User{}
+	}
+
+	log.Println("body: ", body)
+
+	search := User{}
+	err := json.Unmarshal([]byte(body), &search)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return search
+}
+
+func responseGeneration(msg string, status int) events.APIGatewayProxyResponse {
+	return events.APIGatewayProxyResponse{StatusCode: status, Body: "Error: " + msg}
+}
+
+// TO-DO: create a function that handles response returns (more clean and more info/debug info)
+func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+
+	search := unpackRequest(request.Body)
+
+	if search.Username == "" || search.Password == "" {
+		return responseGeneration("field not set", http.StatusBadRequest), nil
+	}
+	item_username := make(map[string]types.AttributeValue)
+	item_username[":username"] = &types.AttributeValueMemberS{Value: search.Username}
+
+	TheInput, err := client.Query(context.Background(), &dynamodb.QueryInput{
+		TableName:                 aws.String(table),
+		IndexName:                 aws.String("username-index"),
+		KeyConditionExpression:    aws.String("username = :username"),
+		ExpressionAttributeValues: item_username,
+	})
+
+	if err != nil {
+		return responseGeneration(err.Error(), http.StatusBadRequest), nil
+	}
+
+	if TheInput.Count == 0 {
+		return responseGeneration("invalid username/password", http.StatusBadRequest), nil
+	}
+
+	if TheInput.Count > 1 {
+		return responseGeneration("more than one username found", http.StatusBadRequest), nil
+	}
+
+	newUser := User{}
+
+	attributevalue.UnmarshalMap(TheInput.Items[0], &newUser)
+
+	check_pass := newUser.Password
+	newUser.Password = ""
+
+	js, err := json.Marshal(newUser)
+
+	if err != nil {
+		return responseGeneration(err.Error(), http.StatusBadRequest), nil
+	}
+
+	//3. Return whole body if it is correct, if not return nothing!
+	if check_pass != search.Password {
+		return responseGeneration("invalid username/password", http.StatusBadRequest), nil
+	}
+
+	return events.APIGatewayProxyResponse{StatusCode: http.StatusOK, Body: string(js), Headers: map[string]string{"content-type": "application/json"}}, nil
+}

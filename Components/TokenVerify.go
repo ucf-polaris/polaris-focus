6c823c7f9a1cebd953f316f4339f5e90f536a420
Kaeden Le <kaedenle@gmail.com>
2023-08-25 23:52:34 -0400

    Added Components File for Proof of Concepts

---
+++ Components/TokenVerify.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"errors"
+	"strings"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+var table string
+
+func main() {
+	lambda.Start(handler)
+}
+
+// Help function to generate an IAM policy
+func generatePolicy(principalId, effect, resource string) events.APIGatewayCustomAuthorizerResponse {
+	authResponse := events.APIGatewayCustomAuthorizerResponse{PrincipalID: principalId}
+
+	if effect != "" && resource != "" {
+		authResponse.PolicyDocument = events.APIGatewayCustomAuthorizerPolicy{
+			Version: "2012-10-17",
+			Statement: []events.IAMPolicyStatement{
+				{
+					Action:   []string{"execute-api:Invoke"},
+					Effect:   effect,
+					Resource: []string{resource},
+				},
+			},
+		}
+	}
+
+	return authResponse
+}
+
+func handler(event events.APIGatewayCustomAuthorizerRequest) (events.APIGatewayCustomAuthorizerResponse, error) {
+	token := event.AuthorizationToken
+	switch strings.ToLower(token) {
+	case "allow":
+		return generatePolicy("user", "Allow", event.MethodArn), nil
+	case "deny":
+		return generatePolicy("user", "Deny", event.MethodArn), nil
+	case "unauthorized":
+		return events.APIGatewayCustomAuthorizerResponse{}, errors.New("Unauthorized") // Return a 401 Unauthorized response
+	default:
+		return events.APIGatewayCustomAuthorizerResponse{}, errors.New("Error: Invalid token")
+	}
+}

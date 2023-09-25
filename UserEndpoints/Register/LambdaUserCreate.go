aaee25d00ecf8c9c6a349ba58715416bd4841b00
Kaeden Le <kaedenle@gmail.com>
2023-09-25 13:05:45 -0400

    Refactor to Register Endpoint

--- UserEndpoints/Register/LambdaUserCreate.go
+++ UserEndpoints/Register/LambdaUserCreate.go
@@ -6,14 +6,12 @@ import (
 	"log"
 	"math/rand"
 	"net/http"
-	"os"
+	"polaris-api/pkg/Helpers"
 	"strconv"
 	"time"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/aws/aws-sdk-go-v2/config"
-	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
 	"github.com/aws/aws-sdk-go/aws"
@@ -27,19 +25,11 @@ var client *dynamodb.Client
 var lambdaClient *lambdaCall.Lambda
 
 func init() {
-	table = os.Getenv("TABLE_NAME")
+	client, table = Helpers.ConstructDynamoHost()
 
 	if table == "" {
 		log.Fatal("missing environment variable TABLE_NAME")
 	}
-
-	//create session for dynamodb
-	cfg, _ := config.LoadDefaultConfig(context.Background())
-	/*config.WithEndpointResolver(aws.EndpointResolverFunc(
-	func(service, region string) (aws.Endpoint, error) {
-		return aws.Endpoint{URL: "http://localhost:8000"}, nil
-	})),*/
-	client = dynamodb.NewFromConfig(cfg)
 
 	//create session for lambda
 	sess := session.Must(session.NewSessionWithOptions(session.Options{
@@ -64,102 +54,30 @@ func produceRandomNDigits(N int) string {
 	return number
 }
 
-func createToken(timeTil int, userID string, mode float64) (string, error) {
-	//-----------------------------------------GET VARIABLES-----------------------------------------
-	JWTFields := make(map[string]interface{})
-
-	JWTFields["timeTil"] = timeTil
-	JWTFields["mode"] = mode
-
-	if userID != "" {
-		JWTFields["UserID"] = userID
-	}
-	//-----------------------------------------PACKAGE RESPONSE-----------------------------------------
-	payload, err := json.Marshal(JWTFields)
-	if err != nil {
-		return "", err
-	}
-
-	resultToken, err := lambdaClient.Invoke(&lambdaCall.InvokeInput{FunctionName: aws.String("token_create"), Payload: payload})
-	if err != nil {
-		return "", err
-	}
-
-	result_json := unpackRequest(string(resultToken.Payload))
-
-	token := result_json["token"].(string)
-
-	return token, nil
-}
-
-func responseGeneration(msg string, status int) (events.APIGatewayProxyResponse, error) {
-	return events.APIGatewayProxyResponse{StatusCode: status, Body: "Error: " + msg}, nil
-}
-
-func unpackRequest(body string) map[string]interface{} {
-	if body == "" {
-		return nil
-	}
-
-	log.Println("body: ", body)
-
-	search := map[string]any{}
-	err := json.Unmarshal([]byte(body), &search)
-
-	if err != nil {
-		panic(err)
-	}
-
-	return search
-}
-
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	//-----------------------------------------EXTRACT REQUIRED FIELDS-----------------------------------------
-	search := unpackRequest(request.Body)
-	var outputError string
+	//-----------------------------------------EXTRACT FIELDS-----------------------------------------
+	search := Helpers.UnpackRequest(request.Body)
 
-	valUser, okUser := search["username"].(string)
-	valPass, okPass := search["password"].(string)
-	valEmail, okEmail := search["email"].(string)
-	valTempPass, okTemp := search["temp_pass"].(string)
-
-	if !okUser || !okPass || !okEmail || !okTemp {
-		if !okUser {
-			outputError += "user "
-		}
-
-		if !okPass {
-			outputError += "password "
-		}
-
-		if !okEmail {
-			outputError += "email "
-		}
-
-		if !okTemp {
-			outputError += "temp pass "
-		}
-
-		return responseGeneration("field not set ("+outputError+")", http.StatusBadRequest)
+	item, _, _, err := Helpers.ExtractFields(
+		[]string{"email", "username", "password", "name", "schedule"},
+		search,
+		false,
+		false,
+	)
+	if err != nil {
+		return Helpers.ResponseGeneration(err.Error(), http.StatusOK)
 	}
 
-	item_email := make(map[string]types.AttributeValue)
-	item_email[":email"] = &types.AttributeValueMemberS{Value: valEmail}
+	//-----------------------------------------EXTRACT FORMATTED EMAIL-----------------------------------------
+	item_email, _, _, err := Helpers.ExtractFields(
+		[]string{"email"},
+		search,
+		true,
+		false,
+	)
 
-	if valTempPass != "potato" {
-		return responseGeneration("temp pass wrong", http.StatusBadRequest)
-	}
-
-	//-----------------------------------------EXTRACT NONREQUIRED FIELDS-----------------------------------------
-	var valName string
-	var valSchedule []interface{}
-	var okName, okSchedule bool
-
-	if valName, okName = search["name"].(string); !okName {
-		valName = ""
-	}
-	if valSchedule, okSchedule = search["schedule"].([]interface{}); !okSchedule {
-		valSchedule = make([]interface{}, 0)
+	if err != nil {
+		return Helpers.ResponseGeneration(err.Error(), http.StatusOK)
 	}
 
 	//-----------------------------------------CHECK QUERY TO PREVENT DUPLICATE EMAILS-----------------------------------------
@@ -171,34 +89,20 @@ func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 	})
 
 	if err != nil {
-		panic(err)
+		return Helpers.ResponseGeneration(err.Error(), http.StatusOK)
 	}
 
 	if TheInput.Count != 0 {
-		return responseGeneration("email already in use", http.StatusBadRequest)
+		return Helpers.ResponseGeneration("email already in use", http.StatusOK)
 	}
 
 	//-----------------------------------------NEW USER CONSTRUCTION-----------------------------------------
-	item := make(map[string]types.AttributeValue)
 	uuid_new := uuid.Must(uuid.NewRandom()).String()
 	code := produceRandomNDigits(5)
 
 	item["UserID"] = &types.AttributeValueMemberS{Value: uuid_new}
-	item["email"] = &types.AttributeValueMemberS{Value: valEmail}
-	item["password"] = &types.AttributeValueMemberS{Value: valPass}
-	item["username"] = &types.AttributeValueMemberS{Value: valUser}
-	item["name"] = &types.AttributeValueMemberS{Value: valName}
 	item["timeTilExpire"] = &types.AttributeValueMemberN{Value: strconv.FormatInt(time.Now().UTC().Add(time.Minute*15).Unix(), 10)}
 	item["verificationCode"] = &types.AttributeValueMemberN{Value: code}
-
-	//put list of strings into correct format
-	av, err := attributevalue.MarshalList(valSchedule)
-
-	if err != nil {
-		return responseGeneration(err.Error(), http.StatusBadRequest)
-	}
-
-	item["schedule"] = &types.AttributeValueMemberL{Value: av}
 
 	//-----------------------------------------PUT UNVERIFIED USER INTO DATABASE-----------------------------------------
 	_, err = client.PutItem(context.Background(), &dynamodb.PutItemInput{
@@ -206,48 +110,54 @@ func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 		Item:      item,
 	})
 	if err != nil {
-		return responseGeneration(err.Error(), http.StatusBadRequest)
+		return Helpers.ResponseGeneration(err.Error(), http.StatusOK)
 	}
 
 	//-----------------------------------------SEND EMAIL CODE-----------------------------------------
 	body := make(map[string]interface{})
-	body["email"] = valEmail
+	body["email"] = search["email"].(string)
 	body["code"], err = strconv.ParseFloat(code, 64)
 
 	if err != nil {
-		return responseGeneration(err.Error(), http.StatusBadRequest)
+		return Helpers.ResponseGeneration(err.Error(), http.StatusOK)
 	}
 
 	payload, err := json.Marshal(body)
 	if err != nil {
-		return responseGeneration(err.Error(), http.StatusBadRequest)
+		return Helpers.ResponseGeneration(err.Error(), http.StatusOK)
 	}
 
-	result, err := lambdaClient.Invoke(&lambdaCall.InvokeInput{FunctionName: aws.String("email_code"), Payload: payload})
-	if err != nil {
-		return responseGeneration("email error: "+err.Error(), http.StatusBadRequest)
+	if !Helpers.IsLambdaLocal() {
+		_, err = lambdaClient.Invoke(&lambdaCall.InvokeInput{FunctionName: aws.String("email_code"), Payload: payload})
+		if err != nil {
+			return Helpers.ResponseGeneration("email error: "+err.Error(), http.StatusOK)
+		}
 	}
 
-	log.Println(result.Payload)
+	//log.Println(result.Payload)
 
 	//-----------------------------------------CREATE TOKEN-----------------------------------------
-	tokenRet, err := createToken(15, uuid_new, 2)
+	tokenRet, err := Helpers.CreateToken(lambdaClient, 15, uuid_new, 2)
 	if err != nil {
-		return responseGeneration(err.Error(), http.StatusBadRequest)
+		return Helpers.ResponseGeneration(err.Error(), http.StatusOK)
 	}
 
 	//-----------------------------------------PACK RETURN VALUES-----------------------------------------
 	ret := make(map[string]interface{})
+
+	/*if Helpers.IsLambdaLocal() {
+		tokenRet = "token"
+		uuid_new = "0"
+	}*/
+
 	ret["token"] = tokenRet
-
 	ret["UserID"] = uuid_new
-
-	ret["email"] = valEmail
+	ret["email"] = search["email"].(string)
 
 	js, err := json.Marshal(ret)
 
 	if err != nil {
-		return responseGeneration(err.Error(), http.StatusBadRequest)
+		return Helpers.ResponseGeneration(err.Error(), http.StatusOK)
 	}
 
 	return events.APIGatewayProxyResponse{StatusCode: http.StatusOK, Body: string(js), Headers: map[string]string{"content-type": "application/json"}}, nil

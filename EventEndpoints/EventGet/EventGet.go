fa9ea6c822dc244ab99cdcb8e665c10a29841a48
Kaeden Le <kaedenle@gmail.com>
2023-09-30 16:04:59 -0400

    EventsGet Changes and Testing

--- EventEndpoints/EventGet/EventGet.go
+++ EventEndpoints/EventGet/EventGet.go
@@ -2,109 +2,137 @@ package main
 
 import (
 	"context"
-	"log"
-	"net/http"
 	"encoding/json"
 	"fmt"
+	"log"
+	"net/http"
+	"polaris-api/pkg/Helpers"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
-	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
-	"github.com/aws/aws-sdk-go-v2/aws"
 )
 
 type EventLocation struct {
-	BuildingLong    float64    `json:"BuildingLong"`
-	BuildingLat     float64    `json:"BuildingLat"`
+	BuildingLong float64 `json:"BuildingLong"`
+	BuildingLat  float64 `json:"BuildingLat"`
 }
 type Event struct {
-	EventID         string          `json:"EventID"`
-	DateTime        string          `json:"dateTime"`
-	Description     string          `json:"description"`
-	Host            string          `json:"host"`
-	Location        EventLocation   `json:"location"`
-	Name            string          `json:"name"`
+	EventID     string        `json:"EventID"`
+	DateTime    string        `json:"dateTime"`
+	Description string        `json:"description"`
+	Host        string        `json:"host"`
+	Location    EventLocation `json:"location"`
+	Name        string        `json:"name"`
 }
 type Payload struct {
-	EventID			string			`json:"EventID"`
+	Name     string `json:"name"`
+	DateTime string `json:"dateTime"`
 }
 
+type Response struct {
+	Events []Event `json:"events"`
+	Tokens Tokens  `json:"tokens"`
+}
+
+type Tokens struct {
+	Token        string `json:"token,omitempty"`
+	RefreshToken string `json:"refreshToken,omitempty"`
+}
+
+// 1. Define as table and client
 var table string
-var db *dynamodb.Client
+var client *dynamodb.Client
 
 func init() {
-	table = "Events"
-	cfg, err := config.LoadDefaultConfig(context.Background())
-	if err != nil {
-		log.Fatalf("Failed to load config, %v", err)
+	// 2. client, table set equal to Helper function
+	client, table = Helpers.ConstructDynamoHost()
+
+	// 3. Error checking on env variables
+	if table == "" {
+		log.Fatal("missing environment variable TABLE_NAME")
 	}
-	db = dynamodb.NewFromConfig(cfg)
 }
 
-func handler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+// 4. Only request in parameters and replace ctx with context.Background()
+func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+
+	// 5. (if need to be protected) Add tokens
+	token, refreshToken, err := Helpers.GetTokens(request)
+	// 6. Proper error checking
+	if err != nil {
+		return Helpers.ResponseGeneration(err.Error(), http.StatusBadRequest)
+	}
+
 	// Create a payload struct
 	var payload Payload
 	// Unmarshal the request body into the payload struct and error check it
-	err := json.Unmarshal([]byte(request.Body), &payload)
+	err = json.Unmarshal([]byte(request.Body), &payload)
 	if err != nil {
-		return events.APIGatewayProxyResponse{
-			StatusCode:	http.StatusBadRequest,
-			Body: 		"Invalid input format",
-		}, nil
+		return Helpers.ResponseGeneration(err.Error(), http.StatusBadRequest)
 	}
+
 	// Extract the ID and use this to get the item from dynamo
-	id := payload.EventID
+	name := payload.Name
+	dateTime := payload.DateTime
 
 	// Fetch the Event in the form of a go struct from the database
-	event, err := getEventByID(ctx, id)
+	event, err := getEventByIndex(context.Background(), name, dateTime)
+
 	// If an error came up, early exit and return the error
 	if err != nil {
-		log.Printf("Error: %v", err)
-		return events.APIGatewayProxyResponse{
-			StatusCode: http.StatusInternalServerError,
-			Body:		fmt.Sprintf("Error fetching Event data: %v", err),
-		}, err
+		return Helpers.ResponseGeneration(fmt.Sprintf("fetching Event data: %v", err), http.StatusBadRequest)
 	}
 
 	// If the Event didn't end up existing, return that information to the caller
 	if event == nil {
-		return events.APIGatewayProxyResponse {
-			StatusCode: http.StatusBadRequest,
-			Body: 		"Event not found in table",
-		}, nil
+		return Helpers.ResponseGeneration("Event not found in table", http.StatusBadRequest)
+	}
+
+	// 7. Pack the tokens with the struct
+	tokens := Tokens{
+		Token:        token,
+		RefreshToken: refreshToken,
+	}
+
+	ret := Response{
+		Events: event,
+		Tokens: tokens,
 	}
 
 	// Convert the Event go struct to a json for return
-	eventJSON, err := json.Marshal(event)
+	eventJSON, err := json.Marshal(ret)
 	// If marshaling failed, early exit
 	if err != nil {
-		return events.APIGatewayProxyResponse{
-			StatusCode: http.StatusInternalServerError,
-			Body: 		"Error converting Event data to JSON",
-		}, err
+		return Helpers.ResponseGeneration("Event not found in table", http.StatusBadRequest)
 	}
 
 	// Return the Event info in the form of a stringified JSON
 	return events.APIGatewayProxyResponse{
 		StatusCode: http.StatusOK,
-		Body: 		string(eventJSON),
+		Body:       string(eventJSON),
+		Headers:    map[string]string{"content-type": "application/json"},
 	}, nil
 }
 
-func getEventByID(ctx context.Context, EventID string) (*Event, error) {
+func getEventByIndex(ctx context.Context, name, dateTime string) ([]Event, error) {
 	// Construct the get item input given the Event ID provided
-	inp := &dynamodb.GetItemInput{
-		TableName: aws.String(table),
-		Key: map[string]types.AttributeValue{
-			"EventID": &types.AttributeValueMemberS{Value: EventID},
+	inp := &dynamodb.QueryInput{
+		TableName:                aws.String(table),
+		IndexName:                aws.String("name-dateTime-index"),
+		KeyConditionExpression:   aws.String("#name = :name AND #dateTime = :dateTime"),
+		ExpressionAttributeNames: map[string]string{"#name": "name", "#dateTime": "dateTime"},
+		ExpressionAttributeValues: map[string]types.AttributeValue{
+			":name":     &types.AttributeValueMemberS{Value: name},
+			":dateTime": &types.AttributeValueMemberS{Value: dateTime},
 		},
 	}
 
 	// Try to query dynamodb with this get item
-	output, err := db.GetItem(ctx, inp)
+	output, err := client.Query(ctx, inp)
 
 	// Return the error if it fails
 	if err != nil {
@@ -112,17 +140,17 @@ func getEventByID(ctx context.Context, EventID string) (*Event, error) {
 	}
 
 	// Return nil if the item didn't end up existing
-	if output.Item == nil {
+	if output.Count == 0 {
 		return nil, nil
 	}
-	log.Printf("Dynamo return: %v", output.Item)
+
 	// construct the go struct from dynamo's item
-	event := &Event{}
-	err = attributevalue.UnmarshalMap(output.Item, event)
+	event := []Event{}
+	err = attributevalue.UnmarshalListOfMaps(output.Items, &event)
 	if err != nil { // if this failed, early exit
 		return nil, err
 	}
-	
+
 	// yay!
 	return event, nil
 }

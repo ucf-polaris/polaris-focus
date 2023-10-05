d4e81806d0bc1d1206d8fadbd4db6062b7e93f99
Kaeden Le <kaedenle@gmail.com>
2023-10-05 19:44:00 -0400

    Building Update Image

--- LocationEndpoints/BuildingUpdate/BuildingUpdate.go
+++ LocationEndpoints/BuildingUpdate/BuildingUpdate.go
@@ -41,7 +41,7 @@ func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 	search := Helpers.UnpackRequest(request.Body)
 
 	items, queryString, mapQuery, err := Helpers.ExtractFields(
-		[]string{"BuildingDesc", "BuildingEvents", "BuildingName"},
+		[]string{"BuildingDesc", "BuildingEvents", "BuildingName", "BuildingImage"},
 		search,
 		true,
 		true)

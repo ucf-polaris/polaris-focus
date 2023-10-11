125fadc309bd77e96a3be598f4fce22fbafb01f6
Kaeden Le <kaedenle@gmail.com>
2023-10-10 21:53:42 -0400

    KnightsConnect Parser Completed

--- PopulateDatabase/PullEvents.py
+++
@@ -1,5 +0,0 @@
-import requests
-r = requests.get('https://knightconnect.campuslabs.com/engage/api/discovery/event/search?endsAfter=2023-10-3&orderByField=endsOn&orderByDirection=ascending&status=Approved')
-response = r.json()
-
-print(response['value'])
